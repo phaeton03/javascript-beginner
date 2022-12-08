b529a0ee5a2c212a4a8920ea7e667e623243516f
nnikolskiy <nnikolskiy@phoenixit.ru>
2022-12-08 11:38:25 +0300

    feature/homework-begginer
    add test and code to homework1

---
+++ src/topics/homework1.js
@@ -0,0 +1,37 @@
+/*
+.В переменных a и b хранятся числа. Написать
+программу, которая выводит в консоль произведение
+и сумму этих чисел.
+ */
+export function multiplyAndSum(a, b) {
+    console.log(a * b, a + b);
+}
+
+/*
+В двух переменных хранятся строки символов.
+Написать программу, которая выведет в консоль
+суммарное количество символов в обоих строках.
+ */
+export function getSymbolsLength(a, b) {
+    console.log(a.length + b.length);
+}
+
+/*
+*Написать программу, которая запрашивает у
+пользователя ввод трёхзначного числа, а потом
+выводит в консоль сумму цифр введённого числа.
+ */
+export function sumThreeDigitNumber() {
+    const threeDigitNumber = prompt('Enter three digit positive number');
+    const notDigitsRegex = /[^\d+]/;
+
+    if (threeDigitNumber.length !== 3 || +threeDigitNumber < 0 || notDigitsRegex.test(threeDigitNumber)) {
+        throw new Error("it must be positive three digit number!!!");
+    }
+
+    const sumDigits = [...threeDigitNumber].reduce((prev, curr) => +prev + +curr);
+
+    console.log(sumDigits);
+}
+
+
